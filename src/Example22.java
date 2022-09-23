b5f08dde9f4e3cb2cd0657abe0811bbcee2ab33a
hvm <aluvid073@ieselcaminas.org>
2022-09-23 17:00:48 +0200

    first commit

---
+++ src/Example22.java
@@ -0,0 +1,15 @@
+public class Example22 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int prod = 1;
+
+        for (int i = 1; i <= 10; i++) {
+            sum = sum + i;
+            prod = prod * i;
+        }
+
+        System.out.println("Addition = " + sum);
+        System.out.println("Multiplication= " + prod);
+    }
+
+}
